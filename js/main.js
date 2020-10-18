09149825604c94e164cfad9d6d687d3e8c7312ad
Ryoko Oka <ryokooka@Raymonds-iMac.local>
2020-10-17 23:44:58 -0400

    tring ajax

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (() => {
 
     // make an AJAX request using the Fetch API
-    fetch('./data/clasData.json')
+    fetch('./data/classData.json')
         .then(res => res.json())
         .then(data => {
 
@@ -13,23 +13,26 @@
 
         handleDataSet();
         }
-
     )
     .catch((err) => {
         console.log(err);
     })
 
-    function handleDataSet(_data) {
+    function handleDataSet(data) {
         panelSection = document.querySelector('.panel-section'),
         panelTemplate = document.querySelector('#panel-template').content;
-
-        debugger;
 
     // loop through the JavaScript object and for each user, make a copy of the user template we find at the bottom of index.html, populate it with the user's data, and put that fresh copy in the users section in index.html
 
     for (let _panel in myData) {
         let panelSection = panelTemplate.cloneNode(true),
-            panelSectionText = panelSection.querySelector('.panel').children;
+            panelText = panel.querySelector('.classData').children;
+
+        let panel = document.querySelector("#panel-template"),
+            courseID = panel.content.querySelector(".text-primary")
+
+        let copy = courseID;
+            copy.innerHTML = "_" + data.coursecode;
 
         panelText[1].innerHTML = myData.coursename;
         panelText[2].innerHTML = myData.coursecode;
@@ -37,9 +40,11 @@
         panelText[4].innerHTML = myData.classtime;
 
         // add this new user to the view
-        panelSection.appendChild(panelSectionText);
+        panelSection.appendChild(panelText);
+
     }
 
     console.log(data);
 }
+
 })();
